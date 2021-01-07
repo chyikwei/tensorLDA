1afbf4e379b32577fb84cae881d913348f9974c8
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2021-01-06 18:06:21 -0800

    update travis setting

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -13,8 +13,9 @@ ls -l
 echo
 if [[ ! -f miniconda.sh ]]
    then
-   wget http://repo.continuum.io/minicondaMiniconda3-4.2.11-Linux-x86_64.sh \
+   wget http://repo.continuum.io/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh \
        -O miniconda.sh
+
    fi
 chmod +x miniconda.sh && ./miniconda.sh -b
 cd ..
